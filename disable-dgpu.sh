2536e922e9ec502abfd2487df03764533a118ed0
Dave R <dave@rouser.org>
2016-09-29 20:23:56 -0400

    shell script to disable and turn off gpu

---
+++ disable-dgpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# disable the discrete gpu
+echo "disabling gpu"
+./disablegpu
+
+# turn off the discrete gpu
+echo "powering down gpu"
+bash -c "echo OFF > /sys/kernel/debug/vgaswitcheroo/switch
